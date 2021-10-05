abe59cab9e82685483f613aabbff9b67112611c4
yoshiwaki <wakk12107@au.com>
2021-10-05 18:57:30 +0900

    Fizzbuzz機能とエラー表示の機能

--- lesson1.js
+++ lesson1.js
@@ -1,35 +1,18 @@
 const button = document.getElementById('button')
 button.addEventListener('click',function(){
 
-
-
 const fizz = document.getElementById('fizz');
-
 const fizzNum = fizz.value;
 const buzz = document.getElementById('buzz');
 const buzzNum = buzz.value;
-// const fizzNum = document.querySelector('#button');
-// const errMsgNone = document.querySelector('.err-msg-none');
 const errMsgFizz = document.querySelector('#message');
 const errMsgBuzz = document.querySelector('#message2');
-
-// function isInt(n){
-//   return Number(n) === n && n % 1 === 0;
-// }
-
-
 
 if (!fizz.value){
   errMsgFizz.textContent = 'FizzNumが入力されていません';
 
-    // alert("FizzNumが未入力です");
     return false;
 
-  // }else if (!typeof fizz.value === string ){
-
-  //   errMsgFizz.textContent = 'FizzNumを整数で入力してください';
-  //   return false;
-  
 }else if (!(fizz.value === fizz.value && fizz.value%1 === 0)){
   errMsgFizz.textContent = 'FizzNumを整数で入力してください';
   return false;
@@ -37,11 +20,8 @@ if (!fizz.value){
 
 }
 
-
 if(!buzz.value){
 
-  
-  // alert("BuzzNumが未入力です");
   errMsgBuzz.textContent = 'BuzzNumが入力されていません';
 
     return false;
@@ -50,15 +30,9 @@ if(!buzz.value){
   return false;
 }
 
-
-
-
 function fizzBuzz(){
   for(let i=1; i<=100; i++){
       if(i%fizzNum=== 0　&& i%buzzNum=== 0){
-        
-   
-  
         const list = document.getElementById('list');
         const lis = document.createElement('li');
         lis.innerHTML = i + "FizzBuzz";
@@ -78,8 +52,6 @@ function fizzBuzz(){
         
         list.appendChild(lis);
       }
-      
-      
   }
 }
 fizzBuzz();

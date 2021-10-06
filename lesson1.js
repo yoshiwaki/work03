8e598cd2c1b0de17abee10bc1cbb73a23e048a51
yoshiwaki <wakk12107@au.com>
2021-10-06 14:32:44 +0900

    改行、アロー関数、querysectorの修正、

--- lesson1.js
+++ lesson1.js
@@ -1,31 +1,28 @@
-const button = document.getElementById('button')
-button.addEventListener('click',function(){
+const button = document.getElementById('button');
+button.addEventListener('click',()=>{
 
 const fizz = document.getElementById('fizz');
 const fizzNum = fizz.value;
 const buzz = document.getElementById('buzz');
 const buzzNum = buzz.value;
-const errMsgFizz = document.querySelector('#message');
-const errMsgBuzz = document.querySelector('#message2');
+const errMsgFizz = document.getElementById('message');
+const errMsgBuzz = document.getElementById('message2');
 
 if (!fizz.value){
   errMsgFizz.textContent = 'FizzNumが入力されていません';
-
     return false;
 
 }else if (!(fizz.value === fizz.value && fizz.value%1 === 0)){
   errMsgFizz.textContent = 'FizzNumを整数で入力してください';
-  return false;
-}
-
-if(!buzz.value){
-
-  errMsgBuzz.textContent = 'BuzzNumが入力されていません';
-
     return false;
+}
+if(!buzz.value){
+  errMsgBuzz.textContent = 'BuzzNumが入力されていません';
+    return false;
+
 }else if (!(buzz.value === buzz.value &&  buzz.value%1 === 0)){
   errMsgBuzz.textContent = 'BuzzNumを整数で入力してください';
-  return false;
+    return false;
 }
 
 function fizzBuzz(){
@@ -34,20 +31,18 @@ function fizzBuzz(){
         const list = document.getElementById('list');
         const lis = document.createElement('li');
         lis.innerHTML = i + "FizzBuzz";
-        
         list.appendChild(lis);
         }
         else if(i%fizzNum === 0) {
           const list = document.getElementById('list');
           const lis = document.createElement('li');
           lis.innerHTML = i + "Fizz";
-          
           list.appendChild(lis);
+
       }else if(i%buzzNum === 0) {
         const list = document.getElementById('list');
         const lis = document.createElement('li');
         lis.innerHTML = i + "Buzz";
-        
         list.appendChild(lis);
       }
   }
